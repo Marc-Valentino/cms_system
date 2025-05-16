332b7a34ef608569a9f6badc250d97253ce1b3d5
Marc-Valentino <rodmarc.villaflores@evsu.edu.ph>
2025-05-16 23:44:21 +0800

    Palo kaynoh

--- dashboards/nurse/medications.php
+++ dashboards/nurse/medications.php
@@ -111,7 +111,7 @@ $medications = [
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/doctor.css">
+    <link rel="stylesheet" href="css/nurse.css">
     <link rel="stylesheet" href="css/medications.css">
 </head>
 <body>
